8cc2d30cd938b95ca46093380ddeb0b995cbb5ab
Lorenzo O. Zimbres <lorenzozimbresfilmmaker@gmail.com>
2023-05-31 21:47:38 -0300

    Fazendo carrossel index

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
-$(document).ready(function(){
-    $(".carousel-categorias").owlCarousel({
-        items: 4,
-        loop: true,
-        nav: true,
-        dots: true,
-    });
+/*-----==Variáveis==-----*/
+const carouselCategorias = document.querySelector(".carousel-categorias");
+const previousItem = document.querySelector(".prvItem");
+const nextItem = document.querySelector(".nxtItem");
+
+/*-----==Eventos==-----*/
+previousItem.addEventListener("click", function(){
+    carouselCategorias.style.left = "-170px";
 });
+nextItem.addEventListener("click", function () {
+    carouselCategorias.style.left = "170px";
+});
+
+/*-----==Funções==-----*/
