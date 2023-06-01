dd6a93b5dad2e862d7f7d3192dc397096db76dde
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-06-01 12:13:08 +0000

    carrossel

--- js/index.js
+++ js/index.js
@@ -4,11 +4,54 @@ const previousItem = document.querySelector(".prvItem");
 const nextItem = document.querySelector(".nxtItem");
 
 /*-----==Eventos==-----*/
-previousItem.addEventListener("click", function(){
-    carouselCategorias.style.left = "-170px";
+window.addEventListener("load", checkLastItem);
+nextItem.addEventListener('click', () => {
+    const currentPosition = parseInt(carouselCategorias.style.left) || 0;
+
+    const newPosition = currentPosition - 160;
+
+    carouselCategorias.style.left = newPosition + "px";
+    
+    checkLastItem();
 });
-nextItem.addEventListener("click", function () {
-    carouselCategorias.style.left = "170px";
+previousItem.addEventListener('click', () => {
+    const currentPosition = parseInt(carouselCategorias.style.left) || 0;
+
+    const newPosition = currentPosition + 160;
+
+    carouselCategorias.style.left = newPosition + "px";
+
+    checkLastItem();
 });
 
 /*-----==Funções==-----*/
+
+//CHATGPT
+
+// Verifica se o carrossel está no último item
+function checkLastItem() {
+    const currentPosition = parseInt(carouselCategorias.style.left) || 0;
+    const widthContainer = document.querySelector(".carousel-categorias-container").scrollWidth;
+    const widthCarousel = carouselCategorias.offsetWidth;
+    const lastVisibleItemPosition = widthContainer - widthCarousel;
+
+    if(currentPosition < -lastVisibleItemPosition){
+        nextItem.classList.add("disabledN");
+        nextItem.disabled = true;
+    }
+    else{
+        nextItem.classList.remove("disabledN");
+        nextItem.disabled = false;
+    }
+
+
+
+    if(currentPosition >= 0){
+        previousItem.classList.add("disabledP");
+        previousItem.disabled = true;
+    }
+    else{
+        previousItem.classList.remove("disabledP");
+        previousItem.disabled = false;
+    }
+}
