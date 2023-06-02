50102ca21e66dda6c8357bfdd4e45f6991c3da7d
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-06-02 19:54:08 +0000

    Cadastro de Eventos finalizados

--- js/sejaProdutor.js
+++ js/sejaProdutor.js
@@ -1,61 +1,108 @@
+/*-----===VARIÁVEIS===-----*/
 const eventForm = document.querySelector('form');
 const eventCard = document.getElementById('eventos-criados');
 const events = [];
 
-eventForm.addEventListener("submit", (event) => {
+/*-----===EVENTOS===-----*/
+eventForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const nameInput = document.getElementById('nome');
-    const dateInput = document.getElementById('data');
-    const typeInput = document.getElementById('tipo');
-    const locationInput = document.getElementById('local');
+    const nome = document.getElementById('nome').value;
+    const data = document.getElementById('data').value;
+    const tipo = document.getElementById('tipo').value;
+    const local = document.getElementById('local').value;
 
-    const name = nameInput.value;
-    const date = dateInput.value;
-    const type = typeInput.value;
-    const location = locationInput.value;
+    var vn = verifyNome(nome);
+    var vd = verifyData(data);
+    var vt = verifyTipo(tipo);
+    var vl = verifyLocal(local);
 
-    const evento = { name, date, type, location };
+
+    const evento = { nome, data, tipo, local };
     events.push(evento);
 
     const card = createEventCard(evento);
-    eventCard.appendChild(card);
+    if (vn && vd && vt && vl) {
+        eventCard.appendChild(card);
 
-    eventForm.reset();
-});
+        eventForm.reset();
+    }
+})
 
-function createEventCard(event) {
+/*-----===FUNÇÕES===-----*/
+function verifyNome(nome) {
+    if (nome.length <= 0) {
+        document.getElementById("nome").classList.add("errado");
+        return false;
+    }
+    else {
+        document.getElementById("nome").classList.remove("errado");
+        return true;
+    }
+}
+function verifyData(data) {
+    if (data.length <= 0) {
+        document.getElementById("data").classList.add("errado");
+        return false;
+    }
+    else {
+        document.getElementById("data").classList.remove("errado");
+        return true;
+    }
+}
+function verifyTipo(tipo) {
+    if (tipo.length <= 0) {
+        document.getElementById("tipo").classList.add("errado");
+        return false;
+    }
+    else {
+        document.getElementById("tipo").classList.remove("errado");
+        return true;
+    }
+}
+function verifyLocal(local) {
+    if (local.length <= 0) {
+        document.getElementById("local").classList.add("errado");
+        return false;
+    }
+    else {
+        document.getElementById("local").classList.remove("errado");
+        return true;
+    }
+}
+
+function createEventCard(evento) {
     const card = document.createElement('div');
     const cardDetalhe = document.createElement("div");
     const cardBtn = document.createElement("div");
-    
+
     card.classList.add('event-card');
     cardDetalhe.classList.add("card-detalhe");
     cardBtn.classList.add("card-btn");
 
     card.appendChild(cardDetalhe);
     card.appendChild(cardBtn);
-    
-    const date = document.createElement('span');
-    date.textContent = event.date;
-    cardDetalhe.appendChild(date);
 
-    const name = document.createElement('h3');
-    name.textContent = event.name;
-    cardDetalhe.appendChild(name);
+    const data = document.createElement('span');
+    data.textContent = evento.data;
+    cardDetalhe.appendChild(data);
 
-    const type = document.createElement('p');
-    type.textContent = event.type;
-    cardDetalhe.appendChild(type);
+    const nome = document.createElement('h3');
+    nome.textContent = evento.nome;
+    cardDetalhe.appendChild(nome);
 
-    const location = document.createElement('p');
-    location.textContent = event.location;
-    cardDetalhe.appendChild(location);
+    const tipo = document.createElement('p');
+    tipo.textContent = evento.tipo;
+    cardDetalhe.appendChild(tipo);
+
+    const local = document.createElement('p');
+    local.textContent = evento.local;
+    cardDetalhe.appendChild(local);
 
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Excluir';
     deleteBtn.addEventListener('click', function () {
-        const index = events.indexOf(event);
+        const index = events.indexOf(evento);
         if (index > -1) {
             events.splice(index, 1);
         }
