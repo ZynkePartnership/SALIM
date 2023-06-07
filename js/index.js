788d9c16d323477b0c5625f2e7cc313fe832a228
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-06-07 11:30:26 -0300

    mudando carrossel

--- js/index.js
+++ js/index.js
@@ -1,53 +1,56 @@
 const carouselVisible = document.querySelector(".carousel-visible");
 const carousel = carouselVisible.querySelector(".carousel");
-const prevButton = document.querySelector(".prvItem");
-const nextButton = document.querySelector(".nxtItem");
+const prevButton = document.querySelectorAll(".prvItem");
+const nextButton = document.querySelectorAll(".nxtItem");
 const itemWidth = carousel.querySelector(".carousel-item").offsetWidth;
 const maxIndex = Math.floor(carousel.scrollWidth / itemWidth) - 1;
 
 let currentIndex = 0;
 
 function moveToNextItem() {
-  if (currentIndex < maxIndex) {
-    currentIndex++;
-    carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-  }
-  checkLastItem();
+    if (currentIndex < maxIndex) {
+        currentIndex++;
+        carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+    }
+    checkLastItem();
 }
 
 function moveToPrevItem() {
-  if (currentIndex > 0) {
-    currentIndex--;
-    carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
-  }
-  checkLastItem();
+    if (currentIndex > 0) {
+        currentIndex--;
+        carousel.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+    }
+    checkLastItem();
 }
 
 function checkLastItem() {
     const containerWidth = carouselVisible.offsetWidth;
     const visibleItems = Math.floor(containerWidth / itemWidth);
     const maxVisibleIndex = Math.floor(carousel.scrollWidth / itemWidth) - visibleItems;
-  
-    if (currentIndex >= maxVisibleIndex) {
-      nextButton.classList.add("disabled");
-      nextButton.disabled = true;
-    } else {
-      nextButton.classList.remove("disabled");
-      nextButton.disabled = false;
-    }
-  
-    if (currentIndex === 0) {
-      prevButton.classList.add("disabled");
-      prevButton.disabled = true;
-    } else {
-      prevButton.classList.remove("disabled");
-      prevButton.disabled = false;
-    }
-  }
-  
 
-nextButton.addEventListener("click", moveToNextItem);
-prevButton.addEventListener("click", moveToPrevItem);
+    if (currentIndex >= maxVisibleIndex) {
+        nextButton.classList.add("disabled");
+        nextButton.disabled = true;
+    } else {
+        nextButton.classList.remove("disabled");
+        nextButton.disabled = false;
+    }
+
+    if (currentIndex === 0) {
+        prevButton.classList.add("disabled");
+        prevButton.disabled = true;
+    } else {
+        prevButton.classList.remove("disabled");
+        prevButton.disabled = false;
+    }
+}
+
+nextButton.forEach(button => {
+    button.addEventListener("click", moveToNextItem);
+});
+prevButton.forEach(button => {
+    button.addEventListener("click", moveToPrevItem);
+});
 
 // Verificar estado inicial do carrossel
 checkLastItem();
